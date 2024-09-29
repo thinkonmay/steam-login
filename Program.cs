4ca04caf72c9fa7a1f93b0eadcc37baa9991e20c
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2024-09-29 16:36:41 +0700

    prototype steam login

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Core;
+
+var acc = new Account
+{
+    Name = "",
+    Password = ""
+};
+
+
+try {
+    Console.WriteLine("start login");
+    WindowUtils.Login(acc,1);
+} catch (Exception e) {
+    Console.WriteLine(e);
+}
